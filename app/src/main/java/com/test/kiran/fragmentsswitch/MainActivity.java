7fa7059cd109de3a5db90b2522b9d049ee4df137
Kiran D Cunha <krndon@gmail.com>
2017-02-16 07:22:22 +0530

    Merge branch 'master' of https://github.com/kirandcunha/FragmentsSwitch
    Conflicts:
    	.gitignore

--- app/src/main/java/com/test/kiran/fragmentsswitch/MainActivity.java
+++ app/src/main/java/com/test/kiran/fragmentsswitch/MainActivity.java
@@ -1,13 +1,39 @@
 package com.test.kiran.fragmentsswitch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.test.kiran.fragmentsswitch.home.BaseHomeFragment;
+import com.test.kiran.fragmentsswitch.home.MenuItemFragment;
+import com.test.kiran.fragmentsswitch.home.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements BaseHomeFragment.OnFragmentInteractionListener, MenuItemFragment.OnListFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        BaseHomeFragment hello = new BaseHomeFragment();
+        fragmentTransaction.add(R.id.home_fragment_container, hello, TAG);
+        fragmentTransaction.addToBackStack(TAG);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.d(TAG, item.toString());
     }
 }
